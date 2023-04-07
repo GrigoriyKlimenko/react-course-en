c05e89175984b09e0b628b24952ef0800c028c92
GrigoryKlimenko <GrigoryKlimenko@artezio.com>
2023-04-07 14:01:25 +0300

    add tests

--- react-ts/src/components/CardsContainer/CardsContainer.test.tsx
+++ react-ts/src/components/CardsContainer/CardsContainer.test.tsx
@@ -1,5 +1,5 @@
-import { describe, it } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { describe, it, vi } from 'vitest';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import { CardsContainer } from './CardsContainer';
 import image from '@assets/no-image.jpg';
@@ -7,11 +7,7 @@ import image from '@assets/no-image.jpg';
 const TEST_DATA = {
   id: 'id12',
   name: 'Firstname Lastname',
-  gender: 'male',
-  city: 'Silent',
   image: image,
-  raceClass: 'Drag racing',
-  date: '1999-02-16',
 };
 
 const TEST_DATA_ARRAY = new Array(10).fill('').map((_, idx) => {
@@ -20,6 +16,23 @@ const TEST_DATA_ARRAY = new Array(10).fill('').map((_, idx) => {
     id: TEST_DATA.id + idx,
   };
 });
+
+global.fetch = vi
+  .fn()
+  .mockResolvedValueOnce({
+    ok: true,
+    json: () =>
+      Promise.resolve({
+        id: '1',
+        image: image,
+        name: 'Nick Qwerty',
+        city: 'Unknown',
+        gender: 'male',
+        date: '1998-01-01',
+        raceClass: 'Drag racing',
+      }),
+  })
+  .mockRejectedValueOnce(new Error('Fetch error'));
 
 describe('CardsContainer component', () => {
   it('Renders 1 card', () => {
@@ -33,5 +46,34 @@ describe('CardsContainer component', () => {
   it('Renders 0 card', () => {
     const { container } = render(<CardsContainer cards={[]} />);
     expect(container.firstChild).toBeEmptyDOMElement();
+  });
+});
+
+describe('Fetch all card data', async () => {
+  it('has all card data', async () => {
+    render(<CardsContainer cards={[TEST_DATA]} />);
+    fireEvent(
+      screen.getByText('Full name: Firstname Lastname'),
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+      })
+    );
+    expect(await screen.findByText('Full name: Nick Qwerty')).toBeVisible();
+    expect(await screen.findByText('City: Unknown')).toBeVisible();
+    expect(await screen.findByText('Date of birth: 1998-01-01')).toBeVisible();
+    expect(await screen.findByText('Gender: male')).toBeVisible();
+    expect(await screen.findByText('Race class: Drag racing')).toBeVisible();
+  });
+  it('get error', async () => {
+    render(<CardsContainer cards={[TEST_DATA]} />);
+    fireEvent(
+      screen.getByText('Full name: Firstname Lastname'),
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+      })
+    );
+    expect(await screen.findByText('Fetch error')).toBeVisible();
   });
 });
--- react-ts/src/pages/Home/Home.test.tsx
+++ react-ts/src/pages/Home/Home.test.tsx
@@ -1,11 +1,24 @@
 import { describe, it, Mock, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { Home } from './Home';
+import image from '@assets/no-image.jpg';
 
 global.fetch = vi.fn(() =>
   Promise.resolve({
     ok: true,
-    json: () => Promise.resolve([]),
+    json: () =>
+      Promise.resolve([
+        {
+          id: '1',
+          image: image,
+          name: 'Nick Qwerty',
+        },
+        {
+          id: '2',
+          image: image,
+          name: 'Mike Render',
+        },
+      ]),
   })
 ) as Mock;
 
@@ -17,5 +30,10 @@ describe('Home page', () => {
         level: 1,
       })
     ).toHaveTextContent('Home');
+  });
+  it('has two cards', async () => {
+    render(<Home />);
+    expect(await screen.findByText('Full name: Nick Qwerty')).toBeVisible();
+    expect(await screen.findByText('Full name: Mike Render')).toBeVisible();
   });
 });
